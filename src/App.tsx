1fd3489dfa8fb92d4db470da3614f2b48049ecc1
danielm-qva <jmastrapa96@gmail.com>
2023-02-26 20:09:15 -0500

    add styles component , add css the login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-       <h1>Hello Word : 🐱‍🏍</h1>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
